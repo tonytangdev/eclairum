3f6a4aa8b2d9faa4f2ad39e8513e5b8e6df9cc75
tonytangdev <tony.tang.developer@gmail.com>
2025-03-07 12:38:26 +0100

    refactor(interfaces): remove unused QuizService and UserService interfaces

--- packages/core/interfaces/quiz-service.interface.ts
+++
@@ -1,7 +0,0 @@
-import { QuizQuestion } from "./llm-service.interface";
-import { QuizGenerationTask } from "../entities/quiz-generation-task";
-
-export interface QuizService {
-  saveQuestions(questions: QuizQuestion[]): Promise<void>;
-  saveQuizGenerationTask(quizGenerationTask: QuizGenerationTask): Promise<void>;
-}
--- packages/core/interfaces/user-service.interface.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "../entities/user";
-
-export interface UserService {
-  createUser(user: User): Promise<User>;
-  getUserByEmail(email: string): Promise<User | null>;
-}
