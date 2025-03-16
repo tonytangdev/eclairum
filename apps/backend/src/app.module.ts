07bbf394e4a98d5c343ccbb01688978f0bc30684
tonytangdev <tony.tang.developer@gmail.com>
2025-03-16 17:32:59 +0100

    fix(backend): add console log for DATABASE_HOST in TypeORM configuration

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -20,25 +20,28 @@ import { ApiKeyGuard } from './common/guards/api-key.guard';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.getOrThrow('DATABASE_HOST'),
-        port: configService.getOrThrow('DATABASE_PORT'),
-        username: configService.getOrThrow('DATABASE_USERNAME'),
-        password: configService.getOrThrow('DATABASE_PASSWORD'),
-        database: configService.getOrThrow('DATABASE_NAME'),
-        synchronize: configService.getOrThrow('DATABASE_SYNCHRONIZE'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        ssl:
-          configService.get('DATABASE_SSL') === 'true'
-            ? { rejectUnauthorized: false }
-            : false,
-        // Force IPv4 connections
-        extra: {
-          family: 4,
-        },
-        logging: configService.get('TYPEORM_LOGGING') === 'true',
-      }),
+      useFactory: (configService: ConfigService) => {
+        console.log('DATABASE_HOST', configService.get('DATABASE_HOST'));
+        return {
+          type: 'postgres',
+          host: configService.getOrThrow('DATABASE_HOST'),
+          port: configService.getOrThrow('DATABASE_PORT'),
+          username: configService.getOrThrow('DATABASE_USERNAME'),
+          password: configService.getOrThrow('DATABASE_PASSWORD'),
+          database: configService.getOrThrow('DATABASE_NAME'),
+          synchronize: configService.getOrThrow('DATABASE_SYNCHRONIZE'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          ssl:
+            configService.get('DATABASE_SSL') === 'true'
+              ? { rejectUnauthorized: false }
+              : false,
+          // Force IPv4 connections
+          extra: {
+            family: 4,
+          },
+          logging: configService.get('TYPEORM_LOGGING') === 'true',
+        };
+      },
     }),
     QuizGenerationTasksModule,
     UsersModule,
