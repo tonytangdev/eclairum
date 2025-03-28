37e88d1ca5a49d2453a3e79ad10031658e26636c
tonytangdev <tony.tang.developer@gmail.com>
2025-03-28 14:36:37 +0100

    feat: create AuthHeader component for managing authentication UI in the header

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -5,16 +5,9 @@ import { ThemeProvider } from "@/components/theme-provider"
 import { AppSidebar } from "@/components/app-sidebar"
 import { SidebarProvider } from "@/components/ui/sidebar"
 import { Footer } from "@/components/footer"
-import { CustomSignInButton } from "@/components/auth/sign-in-button"
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
+import { ClerkProvider, SignedIn } from '@clerk/nextjs'
 import { Toaster } from "@/components/ui/sonner"
-import { NotificationBell } from "@/components/notification-bell"
+import { AuthHeader } from "./_components/auth-header"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -39,17 +32,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <header className="flex justify-end items-center p-4 gap-4 h-16">
-              <SignedOut>
-                <SignInButton mode="modal">
-                  <CustomSignInButton />
-                </SignInButton>
-              </SignedOut>
-              <SignedIn>
-                {/* <div className="flex items-center justify-center gap-4"> */}
-                <NotificationBell />
-                <UserButton />
-                {/* </div> */}
-              </SignedIn>
+              <AuthHeader />
             </header>
             <SidebarProvider>
               <div className="flex flex-1 overflow-hidden">
