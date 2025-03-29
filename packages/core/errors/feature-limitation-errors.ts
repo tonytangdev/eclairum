6b1cfd23283cbcfe2dcd166cd1dcc7739e75dad0
tonytangdev <tony.tang.developer@gmail.com>
2025-03-29 15:25:39 +0100

    refactor: remove unused feature-related error classes and entities

--- packages/core/errors/feature-limitation-errors.ts
+++
@@ -1,7 +0,0 @@
-export class InvalidFeatureLimitationError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = "InvalidFeatureLimitationError";
-    Object.setPrototypeOf(this, InvalidFeatureLimitationError.prototype);
-  }
-}
--- packages/core/errors/index.ts
+++ packages/core/errors/index.ts
@@ -2,4 +2,3 @@ export * from "./quiz-errors";
 export * from "./user-errors";
 export * from "./validation-errors";
 export * from "./user-answer-errors";
-export * from "./feature-errors";
